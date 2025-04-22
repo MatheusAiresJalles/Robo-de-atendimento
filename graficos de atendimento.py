a835d01b02631de89b36cb748c37318859e41717
MatheusAiresJalles <mtjales@gmail.com>
2025-04-22 09:51:08 -0300

    Update graficos de atendimento.py

--- graficos de atendimento.py
+++ graficos de atendimento.py
@@ -10,8 +10,8 @@ problemas = [
     "Outros"
 ]
 
-# Antes do robô: nenhum atendimento fora do horário
-antes_do_robo = [0, 0, 0, 0, 0, 0]
+# Antes do robô: atendimentos realizados manualmente (simulados)
+antes_do_robo = [5, 3, 2, 4, 1, 0]
 
 # Depois do robô: atendimentos realizados automaticamente
 depois_do_robo = [30, 25, 15, 20, 10, 5]
@@ -24,7 +24,7 @@ plt.figure(figsize=(12, 6))
 bar_width = 0.35
 index = range(len(problemas))
 
-plt.bar(index, antes_do_robo, bar_width, color='gray', label='Antes do Robô')
+plt.bar(index, antes_do_robo, bar_width, color='red', label='Antes do Robô')
 plt.bar([i + bar_width for i in index], depois_do_robo, bar_width, color=cores, label='Depois do Robô')
 
 plt.xlabel('Tipo de Problema')
